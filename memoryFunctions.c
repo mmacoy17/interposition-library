856e1352984c2756b79d0841423fa6b2996eb977
mmacoy17 <mattmacoy4@gmail.com>
2016-09-18 21:32:08 -0400

    Implemented the malloc() and free() passthrough functions

--- memoryFunctions.c
+++ memoryFunctions.c
@@ -1,3 +1,25 @@
 #define _GNU_SOURCE
+#include <stdio.h>
 #include <dlfcn.h>
+#include <stddef.h>
+
+typedef void *(*orig_malloc)(size_t size); //Not sure about the need for two pointers or not
+
+void *malloc(size_t size){
+  printf("Passthrough of malloc()");
+  
+  orig_malloc original_malloc;
+  original_malloc = (orig_malloc)dlsym(RTLD_NEXT, "malloc");
+  return original_malloc(size);
+}
+
+typedef void (*orig_free)(void *ptr);
+
+void free(void *ptr){
+  printf("Passthrough of free()");
+  
+  orig_free original_free;
+  original_free = (orig_free)dlsym(RTLD_NEXT, "free");
+  return original_free(*ptr);
+}
 
